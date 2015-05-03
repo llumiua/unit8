5af96bb998d3585714a88f7497866b20b9e974dd
Luz <luzonil@hotmail.com>
2015-05-03 12:11:13 +0200

    segundo commit

--- Asignatura.java
+++ Asignatura.java
@@ -8,5 +8,31 @@ package unit8.unit8;
  *
  */
 public class Asignatura {
-
+	String nombre;
+	double nota;
+	
+	public Asignatura(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public double getNota()
+	{
+		return nota;
+	}
+	
+	public void SetNota(double nota)
+	{
+		this.nota = nota;
+	}
 }
